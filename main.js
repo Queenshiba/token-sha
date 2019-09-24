b1f2743139f6f4ef945a692a7e6df19e375e2fbc
Queenshiba <haruka0111.2a@gmail.com>
2019-09-24 18:52:56 +0900

    made it responsive

---
+++ main.js
@@ -0,0 +1,22 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.fixed-action-btn');
+//     var instances = M.FloatingActionButton.init(elems, options);
+//   });
+
+//   Or with jQuery
+
+//   $(document).ready(function(){
+//     $('.fixed-action-btn').floatingActionButton();
+//   });
+        
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'top',
+      hoverEnabled: false
+             
+  });
+
+  function myFunction() {
+    confirm("準備中");
+  }
